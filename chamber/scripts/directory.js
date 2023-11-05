1bae53792430f9744781ef40a357175bd19521ec
richakwe <42450919+richakwe@users.noreply.github.com>
2023-11-05 21:10:18 +0200

    correction

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://richakwe.github.io/wdd230/chamber/js/data.json';
+const requestURL = 'https://richakwe.github.io/wdd230/chamber/js/members.json';
 
 fetch(requestURL)
   .then(function (response) {
